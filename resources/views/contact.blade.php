be1dc40d5c9fab67834c8a981a305244ac52fb54
Sahan Kaushalya <mkskaushalya@gmail.com>
2024-08-11 00:42:16 +0530

    pages basic setup finised

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,18 @@
+<x-default-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Contact Page') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("Contact Page content") }}
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</x-default-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,17 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+Route::get('about', function () {
+    return view('about');
+})->name('about');
+Route::get('contact', function () {
+    return view('contact');
+})->name('contact');
+Route::get('/', function () {
+    return view('home');
+})->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
