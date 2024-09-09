b425a739b2534b1ef8d91a133ae0a4d49d8ab409
Sahan Kaushalya <mkskaushalya@gmail.com>
2024-09-09 08:15:22 +0530

    Update HallController.php

--- app/Http/Controllers/HallController.php
+++ app/Http/Controllers/HallController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Hall;
+use App\Models\HallBooking;
+use App\Models\HallLocation;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -10,32 +12,22 @@ class HallController extends Controller
 {
     public function index(Request $request): Response
     {
+        $locations = HallLocation::all();
         if (isset($request->location) && isset($request->date) && isset($request->time)) {
             $halls = $this->getHalls($request);
 
         } else {
             $halls = Hall::all();
+
         }
-        return response()->view('halls.index', ['halls' => $halls]);
+        return response()->view('halls.index', ['halls' => $halls, 'locations' => $locations]);
     }
 
     private function getHalls(Request $request)
     {
-        $halls = Hall::query();
-        if (isset($request->location)) {
-            $location = $request->location;
-            $halls->where('location', 'like', "%$location%");
-        }
+        $halls = HallBooking::query();
+        $halls->where('location', $request->location);
 
-        if (isset($request->date)) {
-            $date = $request->date;
-            $halls->whereDate('date', $date);
-        }
-
-        if (isset($request->time)) {
-            $time = $request->time;
-            $halls->whereTime('time', $time);
-        }
 
         return $halls->get();
 
