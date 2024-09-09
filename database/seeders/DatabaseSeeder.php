41563dd32f00636f46f045e70e24285351f97308
Sahan Kaushalya <mkskaushalya@gmail.com>
2024-09-09 08:16:03 +0530

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Hall;
+use App\Models\HallImage;
+use App\Models\Review;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -20,12 +23,28 @@ class DatabaseSeeder extends Seeder
             'firstname' => 'Sahan',
             'lastname' => 'Kaushalya',
             'phone' => '0787520742',
+            'profile_picture' => '/img/avatar.png',
             'address' => 'Kosgama, Sri Lanka',
             'email' => 'mkskaushalya@gmail.com',
             'password' => bcrypt('12345678'),
         ]);
 
+        //Admin User
+        User::factory()->create([
+            'firstname' => 'Admin',
+            'lastname' => 'User',
+            'phone' => '0752575325',
+            'profile_picture' => '/img/avatar.png',
+            'address' => 'Kosgama, Sri Lanka',
+            'email' => 'admin@tute.lk',
+            'password' => bcrypt('12345678'),
+            'usertype' => 'admin',
+        ]);
+
         $this->call(HallLocationSeeder::class);
+        Hall::factory(10)->create();
+        HallImage::factory(50)->create();
+        Review::factory(50)->create();
 
 
     }
