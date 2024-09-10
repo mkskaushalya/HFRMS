6a1797271f541041413229e81ef64e5f6b31c474
Sahan Kaushalya <mkskaushalya@gmail.com>
2024-09-10 14:15:45 +0530

    Update index.blade.php

--- resources/views/halls/index.blade.php
+++ resources/views/halls/index.blade.php
@@ -18,24 +18,32 @@
                         <label for="location">Location</label>
                         <select name="location" id="location">
                             @foreach($locations as $location )
-                                <option value="{{ $location->id }}">{{ $location->location }}</option>
+                                <option
+                                    value="{{ $location->id }}"
+                                    @if($request->location == $location->id) selected @endif>{{ $location->location }}</option>
                             @endforeach
                         </select>
                     </div>
                     <div class="input-box">
-                        <label for="date">Date</label>
-                        <input type="date" value="{{ today()->format("Y-m-d")  }}" min="{{ today()->format("Y-m-d") }}"
-                               name="date" id="date" placeholder="Select Date">
+                        <label for="booking_date">Date</label>
+                        <input type="date"
+                               value="{{ isset($request->booking_date) || $request->booking_date != null ||  $request->booking_date != "" ? $request->booking_date : today()->format("Y-m-d")  }}"
+                               min="{{ today()->format("Y-m-d") }}"
+                               name="booking_date" id="booking_date" placeholder="Select Date">
                     </div>
                     <div class="input-box">
-                        <label for="time">Time</label>
-                        <select name="time" id="time">
-                            <option value="1"><span>Time 1</span></option>
-                            <option value="2"><span>Time 2</span></option>
-                            <option value="3"><span>Time 3</span></option>
-                            <option value="4"><span>Time 4</span></option>
-                        </select>
+                        <label for="start_time">Start Time</label>
+                        <input type="time"
+                               value="{{ isset($request->start_time) || $request->start_time != null ||  $request->start_time != "" ? $request->start_time : "09:00"  }}"
+                               name="start_time" id="start_time" placeholder="Select Time">
                     </div>
+                    <div class="input-box">
+                        <label for="end_time">End Time</label>
+                        <input type="time"
+                               value="{{ isset($request->end_time) || $request->end_time != null ||  $request->end_time != "" ? $request->end_time : "17:00"  }}"
+                               name="end_time" id="end_time" placeholder="Select Time">
+                    </div>
+
                     <div class="input-box">
                         <button type="submit">Search</button>
                     </div>
@@ -56,18 +64,6 @@
             <div class="cont">
                 <div class="heading"><h2>Available Halls</h2></div>
                 <div class="hall-list">
-                    <div class="hall">
-                        <div class="hall-img">
-                            <img src="https://via.placeholder.com/250" alt="Hall Image">
-                        </div>
-                        <div class="hall-info">
-                            <h3>Hall Name</h3>
-                            <p>Location: Location 1</p>
-                            <p>Capacity: 100</p>
-                            <p>Price: $100</p>
-                            <a class="button" href="">Book Now</a>
-                        </div>
-                    </div>
 
                     {{-- Display all halls --}}
                     @foreach($halls as $hall)
@@ -77,10 +73,12 @@
                             </div>
                             <div class="hall-info">
                                 <h3>{{ $hall->name }}</h3>
-                                <p>Location: {{ $hall->location }}</p>
+                                <p>Location: {{ $hall->hallLocation->location }}</p>
                                 <p>Capacity: {{ $hall->capacity }}</p>
                                 <p>Price: ${{ $hall->price }}</p>
-                                <a class="button" href="{{ $hall->id }}">Book Now</a>
+                                <a class="button"
+                                   href="{{ route('halls.show', $hall)  }}{{ $data ? '?booking_date='.$request->booking_date .'&start_time='. $request->start_time.'&end_time='.$request->end_time.'#hall-availability' : ""}}">Book
+                                    Now</a>
                             </div>
                         </div>
 
