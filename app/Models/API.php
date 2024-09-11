ed9eb1ea0574d7d4f1c9264b03dda00e9139b920
Sahan Kaushalya <mkskaushalya@gmail.com>
2024-09-11 07:51:37 +0530

    Create API.php

---
+++ app/Models/API.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class API extends Model
+{
+    public static function ($hall, $request): void
+    {
+        //$checkHallAvailability = Hall::checkHallAvailability($hall, $request->booking_date, $request->start_time, $request->end_time);
+
+    }
+
+}
