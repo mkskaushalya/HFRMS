bb38ea306839292e5ab6ec532f81dc596fed4076
Sahan Kaushalya <mkskaushalya@gmail.com>
2024-09-11 07:52:00 +0530

    Update Review.php

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -9,6 +9,14 @@ class Review extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'rating',
+        'title',
+        'message',
+        'user_id',
+        'hall_id',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
